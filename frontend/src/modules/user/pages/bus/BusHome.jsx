01056e03c447f22bf31ab4789e8404dd02e7e4ba
Om Parteki <omparteki2002@gmail.com>
2026-07-03 12:14:56 +0530

    bus bug fixed

--- frontend/src/modules/user/pages/bus/BusHome.jsx
+++ frontend/src/modules/user/pages/bus/BusHome.jsx
@@ -430,7 +430,7 @@ const BusHome = () => {
             <div className="flex flex-wrap gap-2">
               {quickDates.map((item) => (
                 <button
-                  key={item.value}
+                  key={item.label}
                   type="button"
                   onClick={() => setDate(item.value)}
                   className={`rounded-full px-4 py-2 text-xs font-bold transition-all ${
